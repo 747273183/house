215feef23be2061a9611dcd55611a2173321d743
Administrator <747273183@qq.com>
2019-10-12 11:35:06 +0800

    修改成使用fragment

--- app/src/main/java/com/example/house/controller/MainActivity.java
+++ app/src/main/java/com/example/house/controller/MainActivity.java
@@ -1,30 +1,46 @@
 package com.example.house.controller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.house.R;
+import com.example.house.model.IndexFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = getSupportFragmentManager();
+
+        //加载首页的Fragment
+        switchFragment(new IndexFragment());
+
+
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.commit();
     }
 
     public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.btn_add:
-
+        switch (view.getId()) {
+            case R.id.ll_index:
+                switchFragment(new IndexFragment());
                 break;
-            case R.id.btn_show_standard:
-                Intent intent=new Intent(MainActivity.this,StandardActivity.class);
-                startActivity(intent);
+            case R.id.ll_my:
+                switchFragment(new myFragment());
                 break;
         }
     }
