cbcb5892f1ffa859d007630a3f768d98c29499e1
Administrator <747273183@qq.com>
2019-10-12 18:27:56 +0800

    登录方法封装成功

--- app/src/main/java/com/example/house/model/Constant.java
+++ app/src/main/java/com/example/house/model/Constant.java
@@ -4,4 +4,6 @@ public class Constant {
         public  static  final  String URL= "http://zhapp.t.100help.net/api/";
         public  static  final  String PATH_LOGIN="login/login";
         public static final String   PATH_CHANGE_PWD="member/change_pwd";
+
+        //appkey=zhenghe20190924
 }
